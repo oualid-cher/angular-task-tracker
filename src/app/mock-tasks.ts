1235c291e23d70df2142a6b5b55d0508d70d7185
oualid cherkaoui <ahmedoualidcherkaoui@gmail.com>
2021-05-25 17:54:51 +0100

    start working on tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Interview with someone',
+        day: 'May 27 at 10:00am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Dentist Appointment',
+        day: 'May 27 at 02:00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Dinner with someone',
+        day: 'May 27 at 10:00pm',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: 'Send Something To Someone',
+        day: 'May 28 at 10:00am',
+        reminder: false,
+    },
+]
