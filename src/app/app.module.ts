771865fc5b964445f5ebda69d98714b47f845ce1
oualid cherkaoui <ahmedoualidcherkaoui@gmail.com>
2021-05-26 20:10:06 +0100

    To do list done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClient, HttpClientModule} from '@angular/common/http'
 import {FormsModule} from '@angular/forms'
+import {RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,7 +11,13 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
 
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+
+]
 
 @NgModule({
   declarations: [
@@ -20,12 +27,14 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
   ], 
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs'
 import {Task} from '../../Task'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -11,8 +14,12 @@ export class AddTaskComponent implements OnInit {
   text: string = '';
   day:string = '';
   reminder:boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
